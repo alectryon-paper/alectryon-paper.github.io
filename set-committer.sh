809d6f0250304ad812f934831e9d8217589ff583
Clément Pit-Claudel <clement.pitclaudel@live.com>
2020-11-09 23:04:41 -0500

    Update committer script

--- set-committer.sh
+++ set-committer.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env sh
 git config --local user.name "Anonymous"
 git config --local user.email "anonymous@example.com"
+
+if false; then
+    git config --unset user.name
+    git config --unset user.email
+
+    FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch --env-filter "
+export GIT_COMMITTER_NAME='$(git config user.name)'
+export GIT_COMMITTER_EMAIL='$(git config user.email)'
+export GIT_AUTHOR_NAME='$(git config user.name)'
+export GIT_AUTHOR_EMAIL='$(git config user.email)'
+" --tag-name-filter cat -- --branches --tags
+fi
